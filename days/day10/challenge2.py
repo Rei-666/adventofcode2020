2ae6d6a9ffd9818713ac91e5e86e8f20276806b6
Rei-666 <wszyzdgo@gmail.com>
2020-12-28 14:27:33 +0100

    Add day 10 part 2

--- days/day10/challenge2.py
+++ days/day10/challenge2.py
@@ -1,9 +1,11 @@
 
-with open("./test_input.txt") as file:
+with open("./input.txt") as file:
     text = file.readlines()
 
 text = [int(x[:-1]) for x in text]
 text.sort()
+text.append(text[-1]+3)
+text.insert(0, 0)
 solution = 1
 ps = 0
 for number in range(len(text)):
@@ -11,39 +13,16 @@ for number in range(len(text)):
         difference = text[number + 1] - text[number]
     except IndexError:
         break
-    if difference != 3:
-        for i in range(1, 4):
-            try:
-                difference = text[number+i] - text[number+i-1]
-            except IndexError:
-                break
-            if difference == 1:
-                ps += 1
-        solution *= 4 if ps == 3 else 1
-    ps = 0
+    if difference == 1:
+        ps += 1
+    elif difference == 3:
+        if ps == 2:
+            solution *= 2
+        elif ps == 3:
+            solution *= 4
+        elif ps == 4:
+            solution *= 7
+        ps = 0
+        continue
 
 print(solution)
-
-
-
-print(text)
-'''for number in range(len(text)):
-    for i in range(1, 4):
-        try:
-            difference = text[number+i]-text[number+i-1]
-            print(f"Liczba: {text[number+i]} - {text[number+i-1]}")
-        except IndexError:
-            break
-        if difference == 1:
-            result_1 += 1
-        elif difference == 3:
-            result_3 = 1
-            solutions += result_1+result_3
-            break
-    if result_1 == 3:
-        solutions += 3
-    print(solutions)
-    result_1 = 0
-    result_3 = 0'''
-
-
